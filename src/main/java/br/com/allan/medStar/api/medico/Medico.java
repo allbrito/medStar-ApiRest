52e729e7e3bad26e692dd67da764a88b8b202c4d
Allan <allbdesenvolvedor04@gmail.com>
2023-08-07 13:28:50 -0300

    Renomendo nome de uma variavel escrita errada

--- src/main/java/br/com/allan/medStar/api/medico/Medico.java
+++ src/main/java/br/com/allan/medStar/api/medico/Medico.java
@@ -18,7 +18,7 @@ public class Medico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String emai;
+    private String email;
     private String telefone;
     private String crm;
 
