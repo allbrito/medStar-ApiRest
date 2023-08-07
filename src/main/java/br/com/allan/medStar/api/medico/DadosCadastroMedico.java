0c40be09514887bf4b4a47437fa1d19fdc71a312
Allan <allbdesenvolvedor04@gmail.com>
2023-08-07 17:25:25 -0300

    Adicionando telefone no cadastro do medico

--- src/main/java/br/com/allan/medStar/api/medico/DadosCadastroMedico.java
+++ src/main/java/br/com/allan/medStar/api/medico/DadosCadastroMedico.java
@@ -17,6 +17,9 @@ public record DadosCadastroMedico(
         String email,
 
         @NotBlank
+        String Telefone,
+
+        @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
 
