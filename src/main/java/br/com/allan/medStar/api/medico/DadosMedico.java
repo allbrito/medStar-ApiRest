5be93e7729d3c4e47e4df3e4310c08a06e29c07a
Allan <allbdesenvolvedor04@gmail.com>
2023-08-07 13:32:01 -0300

    Removendo o atributo telefone

--- src/main/java/br/com/allan/medStar/api/medico/DadosMedico.java
+++ src/main/java/br/com/allan/medStar/api/medico/DadosMedico.java
@@ -3,6 +3,6 @@ package br.com.allan.medStar.api.medico;
 import br.com.allan.medStar.api.endereco.DadosEndereco;
 
 public record DadosMedico(String nome, String email,
-                          String telefone, String crm,
-                          Especialidade especialidade, DadosEndereco dadosEndereco) {
+                          String crm, Especialidade especialidade,
+                          DadosEndereco dadosEndereco) {
 }
--- src/main/java/br/com/allan/medStar/api/medico/Medico.java
+++ src/main/java/br/com/allan/medStar/api/medico/Medico.java
@@ -19,7 +19,6 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
-    private String telefone;
     private String crm;
 
     @Enumerated(EnumType.STRING)
