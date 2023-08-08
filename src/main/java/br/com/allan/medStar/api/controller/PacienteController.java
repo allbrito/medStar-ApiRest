addba21838a0205f42c92a888e50d98f5d04d9a6
Allan <allbdesenvolvedor04@gmail.com>
2023-08-08 20:56:59 -0300

    Aplicando a interface PacienteRepository nas classes de paciente

--- src/main/java/br/com/allan/medStar/api/controller/PacienteController.java
+++ src/main/java/br/com/allan/medStar/api/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package br.com.allan.medStar.api.controller;
 
 import br.com.allan.medStar.api.paciente.DadosCadastroPaciente;
+import br.com.allan.medStar.api.paciente.PacienteEntity;
+import br.com.allan.medStar.api.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+    private PacienteRepository repository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new PacienteEntity(dados));
     }
 }
--- src/main/java/br/com/allan/medStar/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/br/com/allan/medStar/api/paciente/DadosCadastroPaciente.java
@@ -1,12 +1,29 @@
 package br.com.allan.medStar.api.paciente;
 
 import br.com.allan.medStar.api.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroPaciente(
 
+        @NotBlank
         String nome,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String telefone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
         String cpf,
+
+        @NotNull
+        @Valid
         DadosEndereco endereco) {
 }
