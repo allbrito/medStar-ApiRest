48ccfe2b8326142c52d726d96ab0207e73e71285
Allan <allbdesenvolvedor04@gmail.com>
2023-08-08 22:15:18 -0300

    Criação do DTO para listar pacientes

--- src/main/java/br/com/allan/medStar/api/controller/PacienteController.java
+++ src/main/java/br/com/allan/medStar/api/controller/PacienteController.java
@@ -1,15 +1,16 @@
 package br.com.allan.medStar.api.controller;
 
 import br.com.allan.medStar.api.paciente.DadosCadastroPaciente;
+import br.com.allan.medStar.api.paciente.DadosListagemPaciente;
 import br.com.allan.medStar.api.paciente.PacienteEntity;
 import br.com.allan.medStar.api.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,5 +23,10 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new PacienteEntity(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 20, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
