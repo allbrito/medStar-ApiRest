be32ad17abae8d32c93c5fc2aaec2a8d740293a9
Allan <allbdesenvolvedor04@gmail.com>
2023-08-09 01:20:13 -0300

    Adicionando o campo de id quando for retornar o objeto

--- src/main/java/br/com/allan/medStar/api/medico/DadosListagemMedico.java
+++ src/main/java/br/com/allan/medStar/api/medico/DadosListagemMedico.java
@@ -1,8 +1,8 @@
 package br.com.allan.medStar.api.medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(MedicoEntity medicoEntity) {
-        this(medicoEntity.getNome(), medicoEntity.getEmail(), medicoEntity.getCrm(), medicoEntity.getEspecialidade());
+        this(medicoEntity.getId(), medicoEntity.getNome(), medicoEntity.getEmail(), medicoEntity.getCrm(), medicoEntity.getEspecialidade());
     }
 }
