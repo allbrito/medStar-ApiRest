8b7d73747158e172e4a16426f270addba7451e99
Allan <allbdesenvolvedor04@gmail.com>
2023-08-14 16:44:12 -0300

    Criando o método para auxiliar a classe de serviço

--- src/main/java/br/com/allan/medStar/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/allan/medStar/api/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.com.allan.medStar.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+
+    UserDetails findByLogin(String login);
 }
