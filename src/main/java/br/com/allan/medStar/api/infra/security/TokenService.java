4ab9d03a5bfa901dbc2c89460dc043e6a3068ce2
Allan <allbdesenvolvedor04@gmail.com>
2023-08-16 10:46:43 -0300

    Adicionando o método para verificar o token enviado

--- src/main/java/br/com/allan/medStar/api/infra/security/TokenService.java
+++ src/main/java/br/com/allan/medStar/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.allan.medStar.api.domain.usuario.UsuarioEntity;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +19,28 @@ public class TokenService {
     private String password;
 
     public String gerarToken(UsuarioEntity usuario) {
-        System.out.println(password);
         try {
-            var algotithm = Algorithm.HMAC256(password);
+            var algorithm = Algorithm.HMAC256(password);
             return JWT.create()
                     .withIssuer("API med.Star")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
-                    .sign(algotithm);
-
+                    .sign(algorithm);
         } catch (JWTCreationException exception) {
-            throw new RuntimeException("Erro ao gerar o token JWT" + exception);
+            throw new RuntimeException("Erro ao gerar o token JWT " + exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(password);
+            return JWT.require(algorithm)
+                    .withIssuer("API med.Star")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado!");
         }
     }
 
