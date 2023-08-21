ed1cdd2f0dd06c8cdbc4fe08a6ad415e57db8bc4
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 10:46:22 -0300

    Criando a entidade JPA de consulta

---
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/ConsultaEntity.java
@@ -0,0 +1,34 @@
+package br.com.allan.medStar.api.domain.consulta;
+
+import br.com.allan.medStar.api.domain.medico.MedicoEntity;
+import br.com.allan.medStar.api.domain.paciente.PacienteEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+public class ConsultaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private MedicoEntity medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private PacienteEntity paciente;
+
+    private LocalDateTime data;
+}
