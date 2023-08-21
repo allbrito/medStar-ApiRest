439d57db6c1098e08df36f84f43c238d94879fb6
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 09:52:28 -0300

    Criando os DTOs de Agendamento e do detalhamento da consulta

---
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,17 @@
+package br.com.allan.medStar.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+
+        Long idMedico,
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        LocalDateTime data
+) {
+}
