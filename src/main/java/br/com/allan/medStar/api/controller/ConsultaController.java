73f7807cfb1092098ad43619ffaec7f0b22e94e9
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 13:43:22 -0300

    Fazendo o método agendar

--- src/main/java/br/com/allan/medStar/api/controller/ConsultaController.java
+++ src/main/java/br/com/allan/medStar/api/controller/ConsultaController.java
@@ -1,21 +1,25 @@
 package br.com.allan.medStar.api.controller;
 
+import br.com.allan.medStar.api.domain.consulta.AgendamentoService;
 import br.com.allan.medStar.api.domain.consulta.DadosAgendamentoConsulta;
+import br.com.allan.medStar.api.domain.consulta.DadosCancelamentoConsulta;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    private AgendamentoService service;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-
+        service.agendar(dados);
+        return ResponseEntity.noContent().build();
     }
+
+
 }
