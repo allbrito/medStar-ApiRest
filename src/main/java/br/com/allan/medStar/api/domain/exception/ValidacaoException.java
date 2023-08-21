b5c035d20e97b6f09a01cc754199a878c7a821c1
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 12:24:07 -0300

    Criando a classe de exception de validação

---
+++ src/main/java/br/com/allan/medStar/api/domain/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package br.com.allan.medStar.api.domain.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
