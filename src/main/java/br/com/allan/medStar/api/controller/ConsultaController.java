a01fbbaf95c178f27fe229daaede39187d9ab44c
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 09:51:45 -0300

    Criando o controller da consulta

---
+++ src/main/java/br/com/allan/medStar/api/controller/ConsultaController.java
@@ -0,0 +1,21 @@
+package br.com.allan.medStar.api.controller;
+
+import br.com.allan.medStar.api.domain.consulta.DadosAgendamentoConsulta;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+
+    }
+}
