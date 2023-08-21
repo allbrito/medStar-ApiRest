316028474f20e257066fc071b321381d02add14e
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 13:18:21 -0300

    Criando um service para o agendamento de consultas

---
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/AgendamentoService.java
@@ -0,0 +1,49 @@
+package br.com.allan.medStar.api.domain.consulta;
+
+import br.com.allan.medStar.api.domain.exception.ValidacaoException;
+import br.com.allan.medStar.api.domain.medico.MedicoEntity;
+import br.com.allan.medStar.api.domain.medico.MedicoRepository;
+import br.com.allan.medStar.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
+        var medico = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var consulta = new ConsultaEntity(null, medico, paciente, dados.data());
+
+        consultaRepository.save(consulta);
+    }
+
+    private MedicoEntity escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for especificado!");
+        }
+
+        return medicoRepository.medicoLivreData(dados.especialidade(), dados.data());
+    }
+}
