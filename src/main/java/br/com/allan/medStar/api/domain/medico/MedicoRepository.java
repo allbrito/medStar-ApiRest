9b45b4a02737f42f2fa98b8ebf18d14e32c64741
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 13:19:18 -0300

    Criando uma query no "MedicoRepository" para pegar de forma aleatória os médicos livres da data do agendamento da consulta

--- src/main/java/br/com/allan/medStar/api/domain/medico/MedicoRepository.java
+++ src/main/java/br/com/allan/medStar/api/domain/medico/MedicoRepository.java
@@ -3,11 +3,25 @@ package br.com.allan.medStar.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
 
     List<MedicoEntity> findAllByAtivoTrue();
     Page<MedicoEntity> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            select m from Medico m where
+            m.ativo = 1 and
+            m.especialidade = :especialidade and
+            m.id not in(
+                select c.medico.id from Consulta c where
+                c.data = :data
+            )
+            order by rand limit 1
+            """)
+    MedicoEntity medicoLivreData(Especialidade especialidade, LocalDateTime data);
 }
