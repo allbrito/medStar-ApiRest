2a0487ba208293f1f652310ce8c369ef3e77fc18
Allan <allbdesenvolvedor04@gmail.com>
2023-08-21 23:16:05 -0300

    Adicionando o método cancelar na classe de serviço

--- src/main/java/br/com/allan/medStar/api/domain/consulta/AgendamentoService.java
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/AgendamentoService.java
@@ -30,9 +30,18 @@ public class AgendamentoService {
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
-        var consulta = new ConsultaEntity(null, medico, paciente, dados.data());
+        var consulta = new ConsultaEntity(null, medico, paciente, dados.data(), null);
 
         consultaRepository.save(consulta);
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 
     private MedicoEntity escolherMedico(DadosAgendamentoConsulta dados) {
