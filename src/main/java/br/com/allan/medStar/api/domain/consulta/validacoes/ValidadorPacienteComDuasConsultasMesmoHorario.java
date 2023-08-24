2bf845a1f8af15008943cd5e8d7ae4f9cc1fc921
Allan <allbdesenvolvedor04@gmail.com>
2023-08-24 12:25:13 -0300

    Criada nova classe para validação de agendamento

---
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/validacoes/ValidadorPacienteComDuasConsultasMesmoHorario.java
@@ -0,0 +1,22 @@
+package br.com.allan.medStar.api.domain.consulta.validacoes;
+
+import br.com.allan.medStar.api.domain.consulta.ConsultaRepository;
+import br.com.allan.medStar.api.domain.consulta.DadosAgendamentoConsulta;
+import br.com.allan.medStar.api.domain.exception.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacienteComDuasConsultasMesmoHorario implements ValidadorAgendamentoConsulta {
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    @Override
+    public void validar(DadosAgendamentoConsulta dados) {
+        var pacienteConsulta = repository.existsByPacienteIdAndData(dados.idPaciente(), dados.data());
+        if (pacienteConsulta) {
+            throw new ValidacaoException("O paciente já possui uma consulta nesse mesmo horário");
+        }
+    }
+}
