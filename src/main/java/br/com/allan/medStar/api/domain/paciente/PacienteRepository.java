81f2d6acc771b7e5397dbd8df1061210750b1bc8
Allan <allbdesenvolvedor04@gmail.com>
2023-08-24 00:02:15 -0300

    Criado o método para verificar se o paciente que teve seu id passado como parâmetro, está ativo

--- src/main/java/br/com/allan/medStar/api/domain/paciente/PacienteRepository.java
+++ src/main/java/br/com/allan/medStar/api/domain/paciente/PacienteRepository.java
@@ -3,6 +3,7 @@ package br.com.allan.medStar.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,11 @@ public interface PacienteRepository extends JpaRepository<PacienteEntity, Long> 
 
     List<PacienteEntity> findAllByAtivoTrue();
     Page<PacienteEntity> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            select p.ativo
+            from Paciente p where 
+            p.id = :idPaciente
+            """)
+    boolean findAtivoById(Long idPaciente);
 }
