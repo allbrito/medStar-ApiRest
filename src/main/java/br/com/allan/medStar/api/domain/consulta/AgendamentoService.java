4f84e987e806f11ded106feb28a070a853111396
Allan <allbdesenvolvedor04@gmail.com>
2023-08-24 11:39:38 -0300

    Colocando o retorno do método agendar como "DadosDetalhamentoConsulta"

--- src/main/java/br/com/allan/medStar/api/domain/consulta/AgendamentoService.java
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/AgendamentoService.java
@@ -25,7 +25,7 @@ public class AgendamentoService {
     @Autowired
     private List<ValidadorAgendamentoConsulta> validadores;
 
-    public void agendar(DadosAgendamentoConsulta dados) {
+    public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados) {
         if (!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException("Id do paciente informado não existe!");
         }
@@ -41,6 +41,7 @@ public class AgendamentoService {
         var consulta = new ConsultaEntity(null, medico, paciente, dados.data(), null);
 
         consultaRepository.save(consulta);
+        return new DadosDetalhamentoConsulta(consulta);
     }
 
     public void cancelar(DadosCancelamentoConsulta dados) {
