c16ea62128645d765530218c98349bf720b27b21
Allan <allbdesenvolvedor04@gmail.com>
2023-08-24 00:02:34 -0300

    Criado o método para verificar se o médico que teve seu id passado como parâmetro, está ativo

--- src/main/java/br/com/allan/medStar/api/domain/medico/MedicoRepository.java
+++ src/main/java/br/com/allan/medStar/api/domain/medico/MedicoRepository.java
@@ -13,15 +13,22 @@ public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
     List<MedicoEntity> findAllByAtivoTrue();
     Page<MedicoEntity> findAllByAtivoTrue(Pageable paginacao);
 
-    @Query("""
+    @Query("""            
             select m from Medico m where
-            m.ativo = 1 and
+            m.ativo = true and
             m.especialidade = :especialidade and
             m.id not in(
                 select c.medico.id from Consulta c where
                 c.data = :data
             )
-            order by rand limit 1
+            order by rand() limit 1
             """)
     MedicoEntity medicoLivreData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Medico m where 
+            m.id = :idMedico
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
