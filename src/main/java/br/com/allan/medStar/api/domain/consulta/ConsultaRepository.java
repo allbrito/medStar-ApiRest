fa1d482056114e5bd8907903a4e9830b54c58276
Allan <allbdesenvolvedor04@gmail.com>
2023-08-24 00:04:50 -0300

    Método criado para saber se o médico que teve seu id passado como parâmetro tem outra consulta na mesma data e hora

--- src/main/java/br/com/allan/medStar/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/br/com/allan/medStar/api/domain/consulta/ConsultaRepository.java
@@ -2,5 +2,9 @@ package br.com.allan.medStar.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Long> {
+
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
